7dbc95956d2d14dab78bf1b20342e49f44d00354
TEAHUI <tamatini@hotmail.fr>
2019-06-05 08:25:12 -1000

    rename all item input ids and names. still broken

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 from lists.models import Item, List
 from django.utils.html import escape
-from lists.forms import ItemForm
+from lists.forms import ItemForm, EMPTY_ITEM_ERROR
 
 class HomePageTest(TestCase):
 
@@ -58,13 +58,21 @@ class ListViewTest(TestCase):
         self.assertRedirects(response, f'/lists/{new_list.id}/')
 
 
-    def test_validation_errors_are_sent_back_to_home_page_template(self):
+    def test_for_invalid_input_renders_home_template(self):
         response = self.client.post('/lists/new', data={"text": ""})
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'home.html')
-        expected_error = escape("You can't have an empty list item")
-        self.assertContains(response, expected_error)
 
+
+    def test_validation_error_are_shown_on_home_page(self):
+        response = self.client.post('/lists/new', data={'text': ''})
+        self.assertContains(response, EMPTY_ITEM_ERROR)
+
+
+    def test_for_invalid_input_passes_form_template(self):
+        response = self.client.post('/lists/new', data={'text': ''})
+        self.assertIsInstance(response.context['form'], ItemForm)
+        
 
     def test_invalid_list_item_arent_saved(self):
         self.client.post('/lists/new', data={'text': ""})
