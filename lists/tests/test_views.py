615540fa39a93f8a42757b7be88c0c8932d89c30
TEAHUI <tamatini@hotmail.fr>
2019-06-09 19:54:11 -1000

    adjust error message

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -2,6 +2,7 @@ from django.test import TestCase
 from lists.models import Item, List
 from django.utils.html import escape
 from lists.forms import ItemForm, EMPTY_ITEM_ERROR
+from unittest import skip
 
 class HomePageTest(TestCase):
 
@@ -114,6 +115,7 @@ class ListViewTest(TestCase):
             data={"text": ""}
         )
 
+
     def test_invalid_input_nothing_saved_to_db(self):
         self.post_invalid_input()
         self.assertEqual(Item.objects.count(), 0)
@@ -124,13 +126,30 @@ class ListViewTest(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, "list.html")
 
+
     def test_for_invalid_input_pass_form_to_template(self):
         response = self.post_invalid_input()
         self.assertIsInstance(response.context['form'], ItemForm)
 
+
     def test_for_invalid_input_show_error_on_page(self):
         response = self.post_invalid_input()
         self.assertContains(response, escape(EMPTY_ITEM_ERROR))
+
+    @skip
+    def test_duplicate_item_validation_errors_end_up_on_list_page(self):
+        list1 = List.objects.create()
+        item1 = Item.objects.create(list=list1, text="textey")
+        response = self.client.post(
+            f'/lists/{list1.id}/',
+            data={'text': 'textey'}
+        )
+
+        expected_error = escape("You've already got this in your list")
+        self.assertContains(response, expected_error)
+        self.assertTemplateUsed(response, 'list.html')
+        self.assertEqual(Item.object.all().count(), 1)
+
 
     def test_display_item_form(self):
         list_ = List.objects.create()
